5a4d86536d426b80bca8e09d7cbffd5b2a1a572e
teejay128 <104542152+Teejay128@users.noreply.github.com>
2023-02-11 23:50:18 +0100

    finished project

--- public/app.js
+++ public/app.js
@@ -13,32 +13,17 @@ const list = new ListTemplate(ul);
 // Event listeners
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    let values;
+    values = [toFrom.value, details.value, amount.valueAsNumber];
     let doc;
     if (type.value == 'invoice') {
-        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber);
+        doc = new Invoice(...values);
     }
     else {
-        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber);
+        doc = new Payment(...values);
     }
     list.render(doc, type.value, 'end');
+    toFrom.value = "";
+    details.value = "";
+    amount.value = "";
 });
-// Enums
-var ResourceType;
-(function (ResourceType) {
-    ResourceType[ResourceType["BOOK"] = 0] = "BOOK";
-    ResourceType[ResourceType["AUTHOR"] = 1] = "AUTHOR";
-    ResourceType[ResourceType["FILM"] = 2] = "FILM";
-    ResourceType[ResourceType["DIRECTOR"] = 3] = "DIRECTOR";
-    ResourceType[ResourceType["PERSON"] = 4] = "PERSON";
-})(ResourceType || (ResourceType = {}));
-const docThree = {
-    uid: 26,
-    resourceType: ResourceType.BOOK,
-    data: { title: "Name of the wind" }
-};
-const docFour = {
-    uid: 45,
-    resourceType: ResourceType.PERSON,
-    data: { name: "Aribad" }
-};
-console.log(docThree, docFour);
--- src/app.ts
+++ src/app.ts
@@ -19,38 +19,19 @@ const list = new ListTemplate(ul)
 form.addEventListener('submit', (e) => {
     e.preventDefault()
 
+    let values: [string, string, number]
+    values = [toFrom.value, details.value, amount.valueAsNumber]
+
     let doc: HasFormatter;
     if(type.value == 'invoice'){
-        doc = new Invoice(toFrom.value, details.value, amount.valueAsNumber)
+        doc = new Invoice(...values)
     } else {
-        doc = new Payment(toFrom.value, details.value, amount.valueAsNumber)
+        doc = new Payment(...values)
     }
 
     list.render(doc, type.value, 'end')
+
+    toFrom.value = ""
+    details.value = ""
+    amount.value = ""
 })
-
-// Enums
-enum ResourceType { BOOK, AUTHOR, FILM, DIRECTOR, PERSON }
-
-
-
-interface Resource <T> {
-    uid: number,
-    resourceType: ResourceType
-    data: T
-}
-
-const docThree: Resource<object> = {
-    uid: 26,
-    resourceType: ResourceType.BOOK,
-    data: { title: "Name of the wind" }
-}
-
-const docFour: Resource<object> = {
-    uid: 45,
-    resourceType: ResourceType.PERSON,
-    data: { name: "Aribad" }
-}
-
-console.log(docThree, docFour)
-
